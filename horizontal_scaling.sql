544659b72ccead9db0270e483ada757a0979b75e
Francis Joseph <francis2241998@gmail.com>
2022-07-05 13:38:26 +0530

    Update horizontal_scaling.sql

--- horizontal_scaling.sql
+++ horizontal_scaling.sql
@@ -1,9 +1,11 @@
-
-
-
-
-
-
+create table horizontal if not exists(source varcher(50), destination varcher(50))
+insert into 
+   horizontal 
+values('chennai','delhi'),
+   ('goa','chennai'),
+   ('delhi','chennai'),
+   ('chennai','goa'),
+   ('mumbai','chennai')
 
 
 select source, destination 
